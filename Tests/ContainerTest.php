40062c09c29c17b344811893271d74e2e93645e5
Benjamin Eberlei <kontakt@beberlei.de>
2012-07-17 21:10:27 +0200

    Test serialize from Container

--- Tests/ContainerTest.php
+++ Tests/ContainerTest.php
@@ -5,16 +5,30 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
 use Symfony\Component\DependencyInjection\Compiler\ResolveDefinitionTemplatesPass;
 
+use Symfony\Component\Form\Extension\Core\CoreExtension;
 use Symfony\Component\Form\FormFactory;
 use Symfony\Component\Form\FormRegistry;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 use SimpleThings\FormSerializerBundle\DependencyInjection\SimpleThingsFormSerializerExtension;
+use SimpleThings\FormSerializerBundle\Serializer\EncoderRegistry;
+use SimpleThings\FormSerializerBundle\Form\SerializerExtension;
 
 class ContainerTest extends \PHPUnit_Framework_TestCase
 {
     public function testContainer()
     {
-        $factory = new FormFactory(new FormRegistry(array()));
+        $registry = new EncoderRegistry(array(new XmlEncoder, new JsonEncoder));
+        $factory  = new FormFactory(new FormRegistry(array(
+            new CoreExtension(),
+            new SerializerExtension($registry)
+        )));
         $container = new ContainerBuilder(new ParameterBag(array(
             'kernel.debug'       => false,
             'kernel.bundles'     => array(),
@@ -32,7 +46,49 @@ class ContainerTest extends \PHPUnit_Framework_TestCase
         $container->compile();
 
         $this->assertInstanceOf('SimpleThings\FormSerializerBundle\Serializer\FormSerializer', $container->get('simple_things_form_serializer.form_serializer'));
-        $this->assertInstanceOf('SimpleThings\FormSerializerBundle\Serializer\FormSerializer', $container->get('form_serializer'));
+        $this->assertInstanceOf('SimpleThings\FormSerializerBundle\Serializer\FormSerializer', $serializer = $container->get('form_serializer'));
+
+        return $serializer;
+    }
+
+    /**
+     * @depends testContainer
+     */
+    public function testSerializeFromContainer($serializer)
+    {
+        $comment = new Comment;
+        $comment->message = "Test";
+
+        $data = $serializer->serialize($comment, new CommentType(), "xml");
+
+        $this->assertEquals("<?xml version=\"1.0\"?>\n<user><message>Test</message></user>\n", $data);
     }
 }
 
+class Comment
+{
+    public $message;
+}
+
+class CommentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('message', 'text')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => __NAMESPACE__ . '\\Comment',
+            'serialize_xml_name'  => 'user',
+        ));
+    }
+
+    public function getName()
+    {
+        return 'comment';
+    }
+}
